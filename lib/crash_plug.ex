4618485434b5370ca2a5bf6f853f196005c35c4f
Ben Marx <bmarx@bleacherreport.com>
2017-05-26 14:15:58 -0700

    update tests

--- lib/crash_plug.ex
+++ lib/crash_plug.ex
@@ -1,15 +1,14 @@
 defmodule CrashPlug do
-  import Plug.Conn
-
   def init(options) do
     options
   end
 
-  def call(conn, _options) do
+  def call(conn, _options \\ []) do
     present? = File.exists?("crashit.txt")
-    case present? do
-      true -> raise "Crash and burn"
-      false -> conn
+    if present? do
+      raise "Crash and burn"
+    else
+      conn
     end
   end
 end
--- test/crash_plug_test.exs
+++ test/crash_plug_test.exs
@@ -3,17 +3,20 @@ defmodule CrashPlugTest do
   use Plug.Test
 
   test "doesn't do anything if there is no file" do
-    conn = conn(:get, "/")
-    conn = put_status(conn, 200)
-    conn = CrashPlug.call(conn, [])
+    conn =
+      conn(:get, "/")
+      |> put_status(200)
+      |> CrashPlug.call
+
     assert conn.status == 200
   end
 
   test "returns 500 when file is present" do
     File.touch("crashit.txt")
-    conn = conn(:get, "/")
-    conn = CrashPlug.call(conn, [])
-    assert conn.status == 500
+    assert_raise RuntimeError, "Crash and burn", fn ->
+      conn(:get, "/")
+      |> CrashPlug.call
+    end
     File.rm("crashit.txt")
   end
 end
